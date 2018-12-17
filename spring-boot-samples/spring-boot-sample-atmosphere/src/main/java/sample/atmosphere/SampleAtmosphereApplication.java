9938d2b158b3cbba31921a73746ccede1928ca81
Carl Larsson <carl.larsson@diadrom.se>
2018-12-17 15:29:27 +0100

    sdsafdas

--- spring-boot-samples/spring-boot-sample-atmosphere/src/main/java/sample/atmosphere/SampleAtmosphereApplication.java
+++ spring-boot-samples/spring-boot-sample-atmosphere/src/main/java/sample/atmosphere/SampleAtmosphereApplication.java
@@ -81,7 +81,7 @@ public class SampleAtmosphereApplication {
 
 	public static void main(String[] args) throws Exception {
 		SpringApplication.run(SampleAtmosphereApplication.class, args);
-		System.out.print("Hello Wlksdflksjflksld");
+		System.out.print("Hello World");
 	}
 
 }
